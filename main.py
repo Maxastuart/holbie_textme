7015df61dfa4e47aeefb9aa01ebfb099d688b638
maxastuart <maxastuart@gmail.com>
2020-02-21 11:29:05 -0800

    requirements & database bits

--- main.py
+++ main.py
@@ -14,22 +14,39 @@
 
 # [START gae_python37_render_template]
 import datetime
-
 from flask import Flask, render_template
+from google.cloud import datastore
+
 
 app = Flask(__name__)
+datastore_client = datastore.Client()
 
+def store_time(dt):
+    entity = datastore.Entity(key=datastore_client.key('visit'))
+    entity.update({
+        'timestamp': dt
+    })
+
+    datastore_client.put(entity)
+
+
+def fetch_times(limit):
+    query = datastore_client.query(kind='visit')
+    query.order = ['-timestamp']
+
+    times = query.fetch(limit=limit)
+
+    return times
 
 @app.route('/')
 def root():
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps.
-    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
-                   datetime.datetime(2018, 1, 2, 10, 30, 0),
-                   datetime.datetime(2018, 1, 3, 11, 0, 0),
-                   ]
+    # Store the current access time in Datastore.
+    store_time(datetime.datetime.now())
 
-    return render_template('index.html', times=dummy_times)
+    # Fetch the most recent 10 access times from Datastore.
+    times = fetch_times(10)
+
+    return render_template('index.html', times=times)
 
 
 if __name__ == '__main__':
